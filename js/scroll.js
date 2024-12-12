ecbf3136c8f8e5b8a3bb0a03f8e19c655cdebba3
DevGabriel0402 <gabriel.profissional01@gmail.com>
2024-12-12 14:06:48 -0300

    Inserindo Seção Main Inicial

--- js/scroll.js
+++ js/scroll.js
@@ -2,8 +2,8 @@ window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY
     if (scrollPosition > 0) {
         document.getElementById('header').style.position = 'fixed'
-        document.getElementById('header').style.zIndex = '999'
-        document.getElementById('header').style.top = '0'
+        document.getElementById('header').style.zIndex = '99'
+        document.getElementById('header').style.top = '-2px !important'
         document.getElementById('header').style.boxShadow = 'rgba(33, 35, 38, 0.1) 0px 10px 10px -10px'
     } else {
         document.getElementById('header').style.position = 'relative'
