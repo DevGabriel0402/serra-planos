f1c75479397b21537762b07373ae5daaf7bfde98
Gabriel Lucas <gabriellucas2301@gmail.com>
2024-12-12 18:02:51 -0300

    Inserindo animacoes de opacidade no header e ajeitando o icone de fechar o menu bar

--- js/scroll.js
+++ js/scroll.js
@@ -1,13 +1,16 @@
 window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY
-    if (scrollPosition > 0) {
+    if (scrollPosition > 20) {
         document.getElementById('header').style.position = 'fixed'
         document.getElementById('header').style.zIndex = '99'
         document.getElementById('header').style.top = '-2px !important'
         document.getElementById('header').style.boxShadow = 'rgba(33, 35, 38, 0.1) 0px 10px 10px -10px'
+        document.getElementById('header').style.animation = 'opacityIn .9s forwards'
     } else {
         document.getElementById('header').style.position = 'relative'
         document.getElementById('header').style.boxShadow = 'none'
+        document.getElementById('header').style.animation = 'opacityDown .9s forwards'
+
 
 
     }
