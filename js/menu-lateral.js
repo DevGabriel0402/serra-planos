ba9cc44e207e8af0e4e75fce86d1722fbf9a1489
DevGabriel0402 <gabriel.profissional01@gmail.com>
2024-12-14 20:14:14 -0300

    Atualizando o tamanho do menu lateral.

--- js/menu-lateral.js
+++ js/menu-lateral.js
@@ -5,12 +5,10 @@ const btnClose = document.getElementById('button-close');
 btnOpen.addEventListener('click', () => {
     menuLateral.classList.remove('hidden-menu');
     menuLateral.style.animation = 'moveIn .5s forwards';
-    document.querySelector('body').style.overflowY = 'hidden'
 })
 btnClose.addEventListener('click', () => {
     menuLateral.style.animation = 'moveDown .5s forwards';
     setTimeout(() => {
         menuLateral.classList.add('hidden-menu');
-        document.querySelector('body').style.overflowY = 'auto'
     }, 500);
 })
