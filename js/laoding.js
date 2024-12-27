cb4ff8c83f421a8259cc766f342fe4511eefd940
DevGabriel0402 <gabriel.profissional01@gmail.com>
2024-12-27 14:46:55 -0300

    Inserindo as informacoes da tab "Nossos Planos"

--- js/laoding.js
+++ js/laoding.js
@@ -6,7 +6,7 @@ window.addEventListener('load', () => {
         document.getElementById('footer').classList.remove('hidden')
     }, 1000)
 
-    if(window.innerWidth <= 790){
+    if (window.innerWidth <= 790) {
         document.getElementById('button-open').classList.remove('hidden')
     }
 })
@@ -15,3 +15,66 @@ window.addEventListener('load', () => {
 document.getElementById('logo').addEventListener('click', () => {
     window.location.reload()
 })
+
+
+// Barra animada da tab Nossos Planos
+
+const cpTotal = document.getElementById('total')
+const cpParcial = document.getElementById('parcial')
+const cpBarra = document.getElementById('barra-animada')
+const cpTotalItem = document.getElementById('coparticipacao-total')
+const cpParcialItem = document.getElementById('coparticipacao-parcial')
+
+cpTotal.addEventListener('click', () => {
+    cpTotalItem.classList.remove('hidden')
+    cpParcialItem.classList.add('hidden')
+    cpTotalItem.style.animation = 'opacityIn .5s forwards'
+
+})
+
+cpParcial.addEventListener('click', () => {
+    cpTotalItem.classList.add('hidden')
+    cpParcialItem.classList.remove('hidden')
+    cpParcialItem.style.animation = 'opacityIn .5s forwards'
+})
+
+
+
+if (window.innerWidth > 520) {
+    cpTotal.addEventListener('click', () => {
+        cpBarra.style.transform = 'translateX(-10px)'
+        cpBarra.style.width = '222px'
+        cpTotal.style.color = 'var(--azul)'
+        cpParcial.style.color = 'var(--branco)'
+    })
+
+    cpParcial.addEventListener('click', () => {
+
+        cpBarra.style.transform = 'translateX(215px)'
+        cpBarra.style.width = '237px'
+        cpParcial.style.color = 'var(--azul)'
+        cpTotal.style.color = 'var(--branco)'
+
+    })
+}
+else {
+    cpTotal.addEventListener('click', () => {
+        cpBarra.style.transform = 'translateX(-10px)'
+        cpBarra.style.width = '150px'
+        cpTotal.style.color = 'var(--azul)'
+        cpParcial.style.color = 'var(--branco)'
+
+    })
+    cpParcial.addEventListener('click', () => {
+        cpBarra.style.transform = 'translateX(140px)'
+        cpBarra.style.width = '160px'
+        cpParcial.style.color = 'var(--azul)'
+        cpTotal.style.color = 'var(--branco)'
+
+
+    })
+}
+
+
+
+
--- js/menu-lateral.js
+++ js/menu-lateral.js
@@ -5,25 +5,28 @@ const btnClose = document.getElementById('button-close');
 btnOpen.addEventListener('click', () => {
     menuLateral.classList.remove('hidden-menu');
     menuLateral.style.animation = 'moveIn .5s forwards';
-    btnClose.classList.remove('hidden')
     btnOpen.classList.add('hidden')
+    btnClose.classList.remove('hidden')
+    btnOpen.style.opacity = 0
 
-    if(window.innerWidth <= 400){
-       document.getElementById('body').style.overflow = 'hidden'
+    if (window.innerWidth <= 400) {
+        document.getElementById('body').style.overflow = 'hidden'
     }
 
-    
+
 })
 btnClose.addEventListener('click', () => {
     menuLateral.style.animation = 'moveDown .5s forwards';
     btnClose.classList.add('hidden')
     btnOpen.classList.remove('hidden')
+    btnOpen.style.opacity = 1
+
 
     setTimeout(() => {
         menuLateral.classList.add('hidden-menu');
     }, 500);
 
-    if(window.innerWidth <= 400){
+    if (window.innerWidth <= 400) {
         document.getElementById('body').style.overflow = 'auto'
-     }
+    }
 })
