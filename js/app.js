8231825a26a89d00c6b8ed5bd4f573889ad89a90
DevGabriel0402 <gabriel.profissional01@gmail.com>
2024-12-28 15:28:25 -0300

    Inserindo e atualizando o evento de clique das tab de dos slider

--- js/app.js
+++ js/app.js
@@ -22,191 +22,319 @@ const menuContato = document.getElementById(`menu-contato`)
 
 // Adicionando evento de click na navegação Desktop
 navInicio.addEventListener('click', () => {
-    navInicio.classList.add(`active`)
-    navPlanos.classList.remove(`active`)
-    navRede.classList.remove(`active`)
-    navSobre.classList.remove(`active`)
-    navContato.classList.remove(`active`)
+    document.getElementById('loading').classList.remove('hidden')
 
-    secInicio.classList.remove('hidden')
-    secNossosPlanos.classList.add('hidden')
-    secRede.classList.add('hidden')
-    secSobre.classList.add('hidden')
-    secContato.classList.add('hidden')
+    setTimeout(() => {
+        document.getElementById('loading').classList.add('hidden')
 
-    window.scroll({
-        top: 0,
-        behavior: "smooth",
-    })
+        navInicio.classList.add(`active`)
+        navPlanos.classList.remove(`active`)
+        navRede.classList.remove(`active`)
+        navSobre.classList.remove(`active`)
+        navContato.classList.remove(`active`)
+
+        secInicio.classList.remove('hidden')
+        secNossosPlanos.classList.add('hidden')
+        secRede.classList.add('hidden')
+        secSobre.classList.add('hidden')
+        secContato.classList.add('hidden')
+
+        window.scroll({
+            top: 0,
+            behavior: "smooth",
+        })
+    }, 1750);
+
 })
 
 navPlanos.addEventListener('click', () => {
-    navInicio.classList.remove(`active`)
-    navPlanos.classList.add(`active`)
-    navRede.classList.remove(`active`)
-    navSobre.classList.remove(`active`)
-    navContato.classList.remove(`active`)
+    document.getElementById('loading').classList.remove('hidden')
 
-    secInicio.classList.add('hidden')
-    secNossosPlanos.classList.remove('hidden')
-    secRede.classList.add('hidden')
-    secSobre.classList.add('hidden')
-    secContato.classList.add('hidden')
+    setTimeout(() => {
+        document.getElementById('loading').classList.add('hidden')
 
-    window.scroll({
-        top: 0,
-        behavior: "smooth",
-    })
+        navInicio.classList.remove(`active`)
+        navPlanos.classList.add(`active`)
+        navRede.classList.remove(`active`)
+        navSobre.classList.remove(`active`)
+        navContato.classList.remove(`active`)
+
+        secInicio.classList.add('hidden')
+        secNossosPlanos.classList.remove('hidden')
+        secRede.classList.add('hidden')
+        secSobre.classList.add('hidden')
+        secContato.classList.add('hidden')
+
+        window.scroll({
+            top: 0,
+            behavior: "smooth",
+        })
+    }, 1750);
+
 })
 
 navRede.addEventListener('click', () => {
-    navInicio.classList.remove(`active`)
-    navPlanos.classList.remove(`active`)
-    navRede.classList.add(`active`)
-    navSobre.classList.remove(`active`)
-    navContato.classList.remove(`active`)
+    document.getElementById('loading').classList.remove('hidden')
 
-    secInicio.classList.add('hidden')
-    secNossosPlanos.classList.add('hidden')
-    secRede.classList.remove('hidden')
-    secSobre.classList.add('hidden')
-    secContato.classList.add('hidden')
+    setTimeout(() => {
+        document.getElementById('loading').classList.add('hidden')
 
-    window.scroll({
-        top: 0,
-        behavior: "smooth",
-    })
+        navInicio.classList.remove(`active`)
+        navPlanos.classList.remove(`active`)
+        navRede.classList.add(`active`)
+        navSobre.classList.remove(`active`)
+        navContato.classList.remove(`active`)
+
+        secInicio.classList.add('hidden')
+        secNossosPlanos.classList.add('hidden')
+        secRede.classList.remove('hidden')
+        secSobre.classList.add('hidden')
+        secContato.classList.add('hidden')
+
+        window.scroll({
+            top: 0,
+            behavior: "smooth",
+        })
+    }, 1500);
+
 })
 
 navSobre.addEventListener('click', () => {
-    navInicio.classList.remove(`active`)
-    navPlanos.classList.remove(`active`)
-    navRede.classList.remove(`active`)
-    navSobre.classList.add(`active`)
-    navContato.classList.remove(`active`)
+    document.getElementById('loading').classList.remove('hidden')
 
-    secInicio.classList.add('hidden')
-    secNossosPlanos.classList.add('hidden')
-    secRede.classList.add('hidden')
-    secSobre.classList.remove('hidden')
-    secContato.classList.add('hidden')
+    setTimeout(() => {
+        document.getElementById('loading').classList.add('hidden')
 
-    window.scroll({
-        top: 0,
-        behavior: "smooth",
-    })
+        navInicio.classList.remove(`active`)
+        navPlanos.classList.remove(`active`)
+        navRede.classList.remove(`active`)
+        navSobre.classList.add(`active`)
+        navContato.classList.remove(`active`)
+
+        secInicio.classList.add('hidden')
+        secNossosPlanos.classList.add('hidden')
+        secRede.classList.add('hidden')
+        secSobre.classList.remove('hidden')
+        secContato.classList.add('hidden')
+
+        window.scroll({
+            top: 0,
+            behavior: "smooth",
+        })
+    }, 1500);
+
 })
 
 navContato.addEventListener('click', () => {
-    navInicio.classList.remove(`active`)
-    navPlanos.classList.remove(`active`)
-    navRede.classList.remove(`active`)
-    navSobre.classList.remove(`active`)
-    navContato.classList.add(`active`)
+    document.getElementById('loading').classList.remove('hidden')
 
-    secInicio.classList.add('hidden')
-    secNossosPlanos.classList.add('hidden')
-    secRede.classList.add('hidden')
-    secSobre.classList.add('hidden')
-    secContato.classList.remove('hidden')
+    setTimeout(() => {
+        document.getElementById('loading').classList.add('hidden')
 
-    window.scroll({
-        top: 0,
-        behavior: "smooth",
-    })
+        navInicio.classList.remove(`active`)
+        navPlanos.classList.remove(`active`)
+        navRede.classList.remove(`active`)
+        navSobre.classList.remove(`active`)
+        navContato.classList.add(`active`)
+
+        secInicio.classList.add('hidden')
+        secNossosPlanos.classList.add('hidden')
+        secRede.classList.add('hidden')
+        secSobre.classList.add('hidden')
+        secContato.classList.remove('hidden')
+
+        window.scroll({
+            top: 0,
+            behavior: "smooth",
+        })
+    }, 1500);
+
 })
 
 // Adicionando evento de click na navegação Mobile (Menu Lateral)
 menuInicio.addEventListener('click', () => {
-    navInicio.classList.add(`active`)
-    navPlanos.classList.remove(`active`)
-    navRede.classList.remove(`active`)
-    navSobre.classList.remove(`active`)
-    navContato.classList.remove(`active`)
+    document.getElementById('loading').classList.remove('hidden')
+    menuLateral.classList.add('hidden-menu');
+    btnClose.classList.add('hidden')
 
-    secInicio.classList.remove('hidden')
-    secNossosPlanos.classList.add('hidden')
-    secRede.classList.add('hidden')
-    secSobre.classList.add('hidden')
-    secContato.classList.add('hidden')
 
-    window.scroll({
-        top: 0,
-        behavior: "smooth",
-    })
+    if (window.innerWidth <= 400) {
+        document.getElementById('body').style.overflow = 'auto'
+    }
+
+    setTimeout(() => {
+        document.getElementById('loading').classList.add('hidden')
+
+        navInicio.classList.add(`active`)
+        navPlanos.classList.remove(`active`)
+        navRede.classList.remove(`active`)
+        navSobre.classList.remove(`active`)
+        navContato.classList.remove(`active`)
+
+        secInicio.classList.remove('hidden')
+        secNossosPlanos.classList.add('hidden')
+        secRede.classList.add('hidden')
+        secSobre.classList.add('hidden')
+        secContato.classList.add('hidden')
+
+        btnOpen.classList.remove('hidden')
+        btnOpen.style.opacity = 1
+
+
+        window.scroll({
+            top: 0,
+            behavior: "smooth",
+        })
+    }, 1500);
+
+
 })
 
 menuPlanos.addEventListener('click', () => {
-    navInicio.classList.remove(`active`)
-    navPlanos.classList.add(`active`)
-    navRede.classList.remove(`active`)
-    navSobre.classList.remove(`active`)
-    navContato.classList.remove(`active`)
+    document.getElementById('loading').classList.remove('hidden')
+    menuLateral.classList.add('hidden-menu');
+    btnClose.classList.add('hidden')
 
-    secInicio.classList.add('hidden')
-    secNossosPlanos.classList.remove('hidden')
-    secRede.classList.add('hidden')
-    secSobre.classList.add('hidden')
-    secContato.classList.add('hidden')
 
-    window.scroll({
-        top: 0,
-        behavior: "smooth",
-    })
+    if (window.innerWidth <= 400) {
+        document.getElementById('body').style.overflow = 'auto'
+    }
+
+    setTimeout(() => {
+        document.getElementById('loading').classList.add('hidden')
+
+        navInicio.classList.remove(`active`)
+        navPlanos.classList.add(`active`)
+        navRede.classList.remove(`active`)
+        navSobre.classList.remove(`active`)
+        navContato.classList.remove(`active`)
+
+        secInicio.classList.add('hidden')
+        secNossosPlanos.classList.remove('hidden')
+        secRede.classList.add('hidden')
+        secSobre.classList.add('hidden')
+        secContato.classList.add('hidden')
+
+
+        btnOpen.classList.remove('hidden')
+        btnOpen.style.opacity = 1
+
+        window.scroll({
+            top: 0,
+            behavior: "smooth",
+        })
+    }, 1500);
+
 })
 
 menuRede.addEventListener('click', () => {
-    navInicio.classList.remove(`active`)
-    navPlanos.classList.remove(`active`)
-    navRede.classList.add(`active`)
-    navSobre.classList.remove(`active`)
-    navContato.classList.remove(`active`)
+    document.getElementById('loading').classList.remove('hidden')
+    menuLateral.classList.add('hidden-menu');
+    btnClose.classList.add('hidden')
 
-    secInicio.classList.add('hidden')
-    secNossosPlanos.classList.add('hidden')
-    secRede.classList.remove('hidden')
-    secSobre.classList.add('hidden')
-    secContato.classList.add('hidden')
 
-    window.scroll({
-        top: 0,
-        behavior: "smooth",
-    })
+    if (window.innerWidth <= 400) {
+        document.getElementById('body').style.overflow = 'auto'
+    }
+
+    setTimeout(() => {
+        document.getElementById('loading').classList.add('hidden')
+
+        navInicio.classList.remove(`active`)
+        navPlanos.classList.remove(`active`)
+        navRede.classList.add(`active`)
+        navSobre.classList.remove(`active`)
+        navContato.classList.remove(`active`)
+
+        secInicio.classList.add('hidden')
+        secNossosPlanos.classList.add('hidden')
+        secRede.classList.remove('hidden')
+        secSobre.classList.add('hidden')
+        secContato.classList.add('hidden')
+
+
+        btnOpen.classList.remove('hidden')
+        btnOpen.style.opacity = 1
+
+        window.scroll({
+            top: 0,
+            behavior: "smooth",
+        })
+    }, 1500);
+
 })
 
 menuSobre.addEventListener('click', () => {
-    navInicio.classList.remove(`active`)
-    navPlanos.classList.remove(`active`)
-    navRede.classList.remove(`active`)
-    navSobre.classList.add(`active`)
-    navContato.classList.remove(`active`)
+    document.getElementById('loading').classList.remove('hidden')
+    menuLateral.classList.add('hidden-menu')
+    btnClose.classList.add('hidden')
 
-    secInicio.classList.add('hidden')
-    secNossosPlanos.classList.add('hidden')
-    secRede.classList.add('hidden')
-    secSobre.classList.remove('hidden')
-    secContato.classList.add('hidden')
 
-    window.scroll({
-        top: 0,
-        behavior: "smooth",
-    })
+    if (window.innerWidth <= 400) {
+        document.getElementById('body').style.overflow = 'auto'
+    }
+
+    setTimeout(() => {
+        document.getElementById('loading').classList.add('hidden')
+
+        navInicio.classList.remove(`active`)
+        navPlanos.classList.remove(`active`)
+        navRede.classList.remove(`active`)
+        navSobre.classList.add(`active`)
+        navContato.classList.remove(`active`)
+
+        secInicio.classList.add('hidden')
+        secNossosPlanos.classList.add('hidden')
+        secRede.classList.add('hidden')
+        secSobre.classList.remove('hidden')
+        secContato.classList.add('hidden')
+
+
+        btnOpen.classList.remove('hidden')
+        btnOpen.style.opacity = 1
+
+        window.scroll({
+            top: 0,
+            behavior: "smooth",
+        })
+    }, 1500);
+
+
 })
 
 menuContato.addEventListener('click', () => {
-    navInicio.classList.remove(`active`)
-    navPlanos.classList.remove(`active`)
-    navRede.classList.remove(`active`)
-    navSobre.classList.remove(`active`)
-    navContato.classList.add(`active`)
+    document.getElementById('loading').classList.remove('hidden')
+    menuLateral.classList.add('hidden-menu');
+    btnClose.classList.add('hidden')
 
-    secInicio.classList.add('hidden')
-    secNossosPlanos.classList.add('hidden')
-    secRede.classList.add('hidden')
-    secSobre.classList.add('hidden')
-    secContato.classList.remove('hidden')
-    window.scroll({
-        top: 0,
-        behavior: "smooth",
-    })
+
+    if (window.innerWidth <= 400) {
+        document.getElementById('body').style.overflow = 'auto'
+    }
+
+    setTimeout(() => {
+        document.getElementById('loading').classList.add('hidden')
+
+        navInicio.classList.remove(`active`)
+        navPlanos.classList.remove(`active`)
+        navRede.classList.remove(`active`)
+        navSobre.classList.remove(`active`)
+        navContato.classList.add(`active`)
+
+        secInicio.classList.add('hidden')
+        secNossosPlanos.classList.add('hidden')
+        secRede.classList.add('hidden')
+        secSobre.classList.add('hidden')
+        secContato.classList.remove('hidden')
+
+
+        btnOpen.classList.remove('hidden')
+        btnOpen.style.opacity = 1
+
+        window.scroll({
+            top: 0,
+            behavior: "smooth",
+        })
+    }, 1500);
+
 })
--- js/swiper.js
+++ js/swiper.js
@@ -33,3 +33,54 @@ var swiper = new Swiper(".swiper-depoimentos", {
   },
 });
 
+document.getElementById('slider-1').addEventListener('click', () => {
+  document.getElementById('loading').classList.remove('hidden')
+
+  setTimeout(() => {
+    document.getElementById('loading').classList.add('hidden')
+
+    navInicio.classList.remove(`active`)
+    navPlanos.classList.add(`active`)
+    navRede.classList.remove(`active`)
+    navSobre.classList.remove(`active`)
+    navContato.classList.remove(`active`)
+
+    secInicio.classList.add('hidden')
+    secNossosPlanos.classList.remove('hidden')
+    secRede.classList.add('hidden')
+    secSobre.classList.add('hidden')
+    secContato.classList.add('hidden')
+
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    })
+  }, 1500);
+
+})
+
+document.getElementById('slider-2').addEventListener('click', () => {
+  document.getElementById('loading').classList.remove('hidden')
+
+  setTimeout(() => {
+    document.getElementById('loading').classList.add('hidden')
+
+    navInicio.classList.remove(`active`)
+    navPlanos.classList.remove(`active`)
+    navRede.classList.add(`active`)
+    navSobre.classList.remove(`active`)
+    navContato.classList.remove(`active`)
+
+    secInicio.classList.add('hidden')
+    secNossosPlanos.classList.add('hidden')
+    secRede.classList.remove('hidden')
+    secSobre.classList.add('hidden')
+    secContato.classList.add('hidden')
+
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    })
+  }, 1500);
+
+})
