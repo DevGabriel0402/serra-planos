54d25e57a8a4d04b4cccd25849496317e6b76121
DevGabriel0402 <gabriel.profissional01@gmail.com>
2025-01-07 13:08:44 -0300

    Atualizando formatacao do codigo

--- js/scroll.js
+++ js/scroll.js
@@ -1,22 +1,21 @@
-window.addEventListener('scroll', () => {
-    const scrollPosition = window.scrollY
+window.addEventListener("scroll", () => {
+    const scrollPosition = window.scrollY;
     if (scrollPosition > 50) {
-        document.getElementById('header').style.position = 'fixed'
-        document.getElementById('header').style.zIndex = '99'
-        document.getElementById('header').style.boxShadow = 'rgba(33, 35, 38, 0.1) 0px 10px 10px -10px'
-        document.getElementById('header').style.animation = 'upIn .5s ease'
-
+        document.getElementById("header").style.position = "fixed";
+        document.getElementById("header").style.zIndex = "99";
+        document.getElementById("header").style.boxShadow =
+            "rgba(33, 35, 38, 0.1) 0px 10px 10px -10px";
+        document.getElementById("header").style.animation = "upIn .5s ease";
     } else {
-        document.getElementById('header').style.position = 'relative'
-        document.getElementById('header').style.boxShadow = 'none'
-        document.getElementById('header').style.animation = 'upDown .5s ease'
+        document.getElementById("header").style.position = "relative";
+        document.getElementById("header").style.boxShadow = "none";
+        document.getElementById("header").style.animation = "upDown .5s ease";
     }
-})
-
+});
 
 // Inserindo ano atual no footer
 
-let hoje = new Date()
-let anoAtual = hoje.getFullYear()
-const ano = document.getElementById('ano')
-ano.innerText = anoAtual
+let hoje = new Date();
+let anoAtual = hoje.getFullYear();
+const ano = document.getElementById("ano");
+ano.innerText = anoAtual;
